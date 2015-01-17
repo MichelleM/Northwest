2a676c6356106185316eaeaf2b12e2e0d1635dbf
Michelle M <michellem@iqmetrix.com>
2015-01-17 11:50:55 -0600

    trying to fix captcha. removing promo page

--- js/email.js
+++ js/email.js
@@ -4,87 +4,69 @@
   */
 $(function() {
 
- $("input,textarea").jqBootstrapValidation(
-    {
-     preventSubmit: true,
-     submitError: function($form, event, errors) {
+ $("input,textarea").jqBootstrapValidation({
+   preventSubmit: true,
+      submitError: function($form, event, errors) {
       // something to have when submit produces an error ?
       // Not decided if I need it yet
-     },
-     submitSuccess: function($form, event) {
+   },
+   submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
-       // get values from FORM
-       var name = $("input#fullName").val();  
-       var address = $("input#address").val();  
-       var phone = $("input#phone").val();  
-       var email = $("input#email").val(); 
-       var comments = $("textarea#comments").val(); 
-       var products = '';
-       var heard = $('select#heard').val();
+      // get values from ORM
+      var name = $("input#fullName").val();  
+      var address = $("input#address").val();  
+      var phone = $("input#phone").val();  
+      var email = $("input#email").val(); 
+      var comments = $("textarea#comments").val(); 
+      var products = '';
+      var heard = $('select#heard').val();
 
 
-       $('checkbox :checked').each(function() {
-          products += $(this).val() + '<br>'
-       });
+      $('checkbox :checked').each(function() {
+        products += $(this).val() + '<br>'
+      });
 
-       var firstName = name; // For Success/Failure Message
-           // Check for white space in name for Success/Fail message
-        if (firstName.indexOf(' ') >= 0) {
-	     firstName = name.split(' ').slice(0, -1).join(' ');
-         }        
-	     $.ajax({
-                url: "./php/emails.php",
-            	type: "POST",
-            	data: {
-                name: name, 
-                email: email, 
-                phone: phone,
-                comments: comments,
-                address: address,
-                products: products,
-                heard: heard,
-                recaptcha_challenge_field: Recaptcha.get_challenge(),
-                recaptcha_response_field: Recaptch.get_response()
-              },
-            	cache: false,
-            	success: function() {  
-            	// Success message
-            	   $('#success').html("<div class='alert alert-success'>");
-            	   $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            		.append( "</button>");
-            	  $('#success > .alert-success')
-            		.append("<strong>Your request has been sent. </strong>");
- 		  $('#success > .alert-success')
- 			.append('</div>');
- 						    
- 		  //clear all fields
- 		  $('#contactForm').trigger("reset");
- 	      },
- 	   error: function() {		
- 		// Fail message
- 		 $('#success').html("<div class='alert alert-danger'>");
-            	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email us directly at <a href='mailto:contactus@northwestresidential.com?Subject=Request a quote'>contactus@northwestresidential.com</a> ? Sorry for the inconvenience!");
- 	        $('#success > .alert-danger').append('</div>');
- 		//clear all fields
- 		$('#contactForm').trigger("reset");
- 	    },
-           })
-         },
-         filter: function() {
-                   return $(this).is(":visible");
-         },
-       });
-
-      $("a[data-toggle=\"tab\"]").click(function(e) {
-                    e.preventDefault();
-                    $(this).tab("show");
-        });
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+       firstName = name.split(' ').slice(0, -1).join(' ');
+      }   
+  
+      $.ajax({
+        url: "./php/emails.php",
+      	type: "POST",
+      	data: {
+          name: name, 
+          email: email, 
+          phone: phone,
+          comments: comments,
+          address: address,
+          products: products,
+          heard: heard,
+          recaptcha_response_field: grecaptcha.getResponse(),
+        },
+      	cache: false,
+      	success: function() {  
+      	  // Success message
+      	  $('#notification').html("<div class='alert alert-success'>");
+      	  $('#notification > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+      		.append( "</button>");
+      	  $('#notification > .alert-success')
+      		.append("<strong>Your request has been sent. </strong>");
+    		  $('#notification > .alert-success')
+    			.append('</div>');
+						    
+    		  //clear all fields
+    		  $('#contactForm').trigger("reset");
+	      },
+	      error: function() {		
+		        // Fail message
+		      $('#notification').html("<div class='alert alert-danger'>");
+          $('#notification > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
+          $('#notification > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email us directly at <a href='mailto:contactus@northwestresidential.com?Subject=Request a quote'>contactus@northwestresidential.com</a> ? Sorry for the inconvenience!");
+	        $('#notification > .alert-danger').append('</div>');
+        }
+      });
+    }
   });
- 
-
-/*When clicking on Full hide fail/success boxes */ 
-$('#name').focus(function() {
-     $('#success').html('');
-  });
+});
--- php/emails.php
+++ php/emails.php
@@ -1,44 +1,41 @@
+
 <?php
-
-require_once('recaptchalib.php');
-  $privatekey = "6Le7sfgSAAAAAGCAIs4m0jxr0NeI32nXc-k3QVyt";
-  $resp = recaptcha_check_answer ($privatekey,
-                                $_SERVER["REMOTE_ADDR"],
-                                $_POST["recaptcha_challenge_field"],
-                                $_POST["recaptcha_response_field"]);
-
-  if (!$resp->is_valid) {
-    // What happens when the CAPTCHA was entered incorrectly
-    die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
-         "(reCAPTCHA said: " . $resp->error . ")");
-  } else {$name = $_POST['name'];
-	$email_address = $_POST['email'];
-	$address = $_POST['address'];
-	$phone = $_POST['phone'];
-	$comments = $_POST['comments'];
-	$heard = $_POST['heard'];
-	$products = $_POST['products'];
+// check if fields passed are empty
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+$comments = $_POST['comments'];
+$products = $_POST['products'];
+$heard = $_POST['heard'];
+$captcha = $_POST['recaptcha_response_field'];
 
 
-	// create email body and send it	
-	//$to = 'contactus@northwestresidential.com'; // put your email
-	$to = 'mlmontford@gmail.com'; // put your email
+// create email body and send it    
+$to = 'milmontford@gmail.com'; // put your email
+$email_subject = "Quote request from:  $name";
+$email_body = "You have received an new quote request. \n\n".
+                "Name: $name\n".
+                "Email: $email_address\n".
+                "Phone: $phone\n".
+                "Address: $address\n".
+                "Comments: $comments\n".
+                "Interested in: $products\n".
+                "Heard about you from: $heard\n";
+$headers = "From: $email_address\n";
+$headers .= "Reply-To: $email_address"; 
 
-	$email_subject = "Quote request from:  $name";
-	$email_body = "You have received a new quote reques. \n\n".
-					  "Name: $name\n
-					  Email: $email_address\n
-					  Address: $address\n
-					  Phone: $phone\n\n
-					  Comments: $comments\n\n
-					  Products they are interested in: $products\n\n
-					  Heard about you via: $heard";
-	$headers = "From: no-reply@northwestresidential.ca\n";
-	$headers .= "Reply-To: $email_address";	
-	mail($to,$email_subject,$email_body,$headers);
-	return true;			
-  }
-</php>
+$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Le7sfgSAAAAAGCAIs4m0jxr0NeI32nXc-k3QVyt&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
 
+//THIS IS NOT WORKING :( i just need to verify that the captcha response was valid. If yes send email. If no return error.
+if($response.success==false)
+{
+  echo '<h2>You are spammer ! Get the @$%K out</h2>';
+}else
+{
+  echo '<h2>Thanks for posting comment.</h2>';
+}
 
-
+mail($to,$email_subject,$email_body,$headers);
+return false;            
+?>
