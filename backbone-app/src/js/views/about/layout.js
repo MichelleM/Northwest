605599f9d1a1d663fbbd57bb0fe1737082ec0f4b
Michelle M <michellemontford@w308-23.academic.rrc.ca>
2015-03-28 14:49:53 -0500

    fixing google reviews

--- backbone-app/src/js/views/about/layout.js
+++ backbone-app/src/js/views/about/layout.js
@@ -6,6 +6,10 @@ define([
     'use strict';
 
     return Marionette.LayoutView.extend({
-        template: Template
+        template: Template,
+
+        onShow: function() {
+        	gapi.post.render("widget-div", {'href' : 'https://plus.google.com/108385852680441972538/posts/fGa9dfBpk8H'} );
+        }
     });
 });
--- backbone-app/src/js/views/services/layout.js
+++ backbone-app/src/js/views/services/layout.js
@@ -5,6 +5,10 @@ define([
     'use strict';
 
     return Marionette.LayoutView.extend({
-        template: Template
+        template: Template,
+
+        onShow: function() {
+        	gapi.post.render("widget-div", {'href' : 'https://plus.google.com/108385852680441972538/posts/bc2Sio4ejE8'} );
+        }
     });
 });
